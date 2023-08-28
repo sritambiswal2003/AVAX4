e8eb1e5069bb56311355666e6cef4bae83b2a501
sritambiswal2003 <139357786+sritambiswal2003@users.noreply.github.com>
2023-08-28 14:01:11 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# DEGEN Token Smart Contract
+
+This is a blockchain project made with Solidity on the Avalanche Network. This program creates an ERC20 Token that can be minted by the owner and transferred. The tokens can be used to redeem NFTs on the network 
+## Description
+
+This program is a smart contract written in Solidity, a programming language used for developing smart contracts. The is an ERC20 Token contract and has functions inherited from the OpenZepplin Library that provide secure standards to create contracts. The contract contains functions like Transfer, Mint, Allow, BalanceOf, Owner, Symbol, Name, etc. On top of that I have created an NFT collection where only owner can add the NFTs and other people can use their DGN token to redeem the NFTs.
+
+## Getting Started
+
+### Executing program
+_Add your private key and Snowtrace API inside the **hardhat.config.js** file._
+  
+- Inside the project directory, in the terminal type: npm i
+- Open two additional terminals in your VS code
+- In the second terminal type: ```npx hardhat node```
+- In the third terminal, type: ```npx hardhat run scripts/deploy.js --network fuji```
+- To verify the smart contract run: ```npx hardhat verify <Contract Address> --network fuji```
+  
+
+## Authors
+
+Indranil Chutia  
---
+++ contracts/DegenToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract DegenToken is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("DEGEN", "DGN") {}
+
+    // Event emitted when NFT is successfully redeemed
+    event NftRedeemed(address indexed redeemer, string nftName, uint256 price);
+
+    // NFT Structure
+    struct NFT {
+        uint256 ID;
+        string nftName;
+        uint256 price;
+    }
+
+    // Mapping an NFT with ID
+    mapping(uint256 => NFT) public nftList;
+    uint256 public nftCount; // NFT Count
+
+    // Constructor to initialize the token
+    constructor() ERC20("DEGEN", "DGN") {}
+
+    // Mint tokens (same as before)
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    // Adding an NFT to the list
+    function addNft(string memory _nftName, uint256 _price) external onlyOwner {
+        nftCount++;
+        NFT memory nft = NFT(nftCount, _nftName, _price);
+        nftList[nftCount] = nft;
+    }
+
+    // Redeeming Tokens for an NFT
+    function redeem(uint256 _ID) external {
+        require(_ID > 0 && _ID <= nftCount, "Invalid NFT ID");
+        NFT memory redeemNft = nftList[_ID];
+
+        require(balanceOf(msg.sender) >= redeemNft.price, "Insufficient Balance");
+
+        _transfer(msg.sender, owner(), redeemNft.price);
+        emit Transfer(msg.sender, address(0), redeemNft.price);
+
+        emit NftRedeemed(msg.sender, redeemNft.nftName, redeemNft.price);
+    }
+}
